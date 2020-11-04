38ce1753194a236d473821d4d026a491e3c760b5
Ahmedsabrymohamed74 <ahmad.s.abdelaziz@gmail.com>
2020-11-04 19:24:00 +0200

    First Commit

---
+++ app/src/main/java/app/AppConfig.java
@@ -0,0 +1,10 @@
+package app;
+
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://192.168.1.7/android_api/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://192.168.1.7/android_api/register.php";
+}
